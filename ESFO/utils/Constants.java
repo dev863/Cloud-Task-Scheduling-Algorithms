85436bd5c82a8787c48b12b47d00f65774947055
Pavithra S <110013771+dev863@users.noreply.github.com>
2023-07-08 22:59:32 +0530

    Create Constants.java

---
+++ ESFO/utils/Constants.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Constants {
+    public static final int NO_OF_TASKS =100; // number of Cloudlets;
+    
+  
+    public static double s=Math.random();
+    public static double t=Math.random();
+    public static double r=Math.random();
+    public static double phi=Math.random()*2*Math.PI;
+    public static final double PP = (s*Math.sin(phi)+t*(Math.cos(phi)))*r;
+    
+}
